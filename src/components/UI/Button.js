bb108bd7136c9f1972c0b3d61b06cce79bea8535
KiHoon Roh <started677@gmail.com>
2022-08-25 18:20:28 +0900

    💄UI: 회원가입 페이지 퍼블리싱

---
+++ src/components/UI/Button.js
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+export const Button = ({ width, height, onClick, disabled, type, children }) => {
+  return (
+    <StyledButton width={width} height={height} onClick={onClick} disabled={disabled} type={type}>
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  padding: 0.4rem 1.6rem;
+  font-size: 1.8rem;
+  color: #4d5159;
+  background-color: #80b2da;
+  border-radius: 4px;
+  border: 1px solid #d1d3d8;
+  cursor: pointer;
+  transition: all 80ms linear;
+  &:hover {
+    box-shadow: 0 0 0 1px #80c0da;
+  }
+  &:disabled {
+    background-color: #f7f8fa;
+    color: #c2c8cc;
+    border-color: #dadce0;
+    cursor: default;
+    &:hover {
+      box-shadow: none;
+    }
+  }
+`;
